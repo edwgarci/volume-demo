85a5808cee94c77418f5ef3f487499b004961d4c
Edwin Garcia <edwgarci@gmail.com>
2018-02-18 20:44:42 -0600

    Update package.sh

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -6,24 +6,24 @@ pwd
 ls -la
 # VERSION=0.0.0
 ls version -la
-VERSION=$(cat version/number)
+VERSION=$(cat ../common-version/number)
 echo $VERSION
 
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export PATH=$JAVA_HOME/bin:$PATH
+# export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+# export PATH=$JAVA_HOME/bin:$PATH
 
-export M2_HOME=/usr/local/apache-maven/apache-maven-3.3.9
-export M2=$M2_HOME/bin
-export MAVEN_OPTS="-Xms256m -Xmx512m"
-export PATH=$M2:$PATH
+# export M2_HOME=/usr/local/apache-maven/apache-maven-3.3.9
+# export M2=$M2_HOME/bin
+# export MAVEN_OPTS="-Xms256m -Xmx512m"
+# export PATH=$M2:$PATH
 
 cd git-assets/
-mvn -DskipTests=true package
+# mvn -DskipTests=true package
 
 echo "saving file with version number: $VERSION"
-pwd
-ls -la
-ls target -la
-ls ../ -la
-chmod -R 755 ../$CF_APP-releases/
-cp target/*.jar ../$CF_APP-releases/$CF_APP-$VERSION.jar
+# pwd
+# ls -la
+# ls target -la
+# ls ../ -la
+# chmod -R 755 ../$CF_APP-releases/
+# cp target/*.jar ../$CF_APP-releases/$CF_APP-$VERSION.jar
